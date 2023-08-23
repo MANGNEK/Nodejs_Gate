ea2cddf6ae7b7e437e9dbb36352ef660624b0d4f
MANGNEK <trungnguyen.kc5@gmail.com>
2023-08-23 14:01:03 +0700

    f
    f

--- API NODEJS/api/router/sound.js
+++ API NODEJS/api/router/sound.js
@@ -3,18 +3,33 @@ const router = express.Router();
 
 router.get('/',(req,res,next) =>{
 
-    const play = require('play-sound')({player: null});
-    const soundFilePath = './Sound/hello.mp3'; 
-    play.play(soundFilePath, (err) => {
-      if (err) {
-        res.status(401).json({
-            message: 'lõi'
-        });
-      } else {
-        res.status(200).json({
-            message: 'đang chạy'
-        })
-      }
-    }); 
+const player = require('play-sound')({ player: null });
+const soundFilePath = './Sound/hello.mp3'; 
+
+player.play(soundFilePath, (err) => {
+    if (err) {
+      res.status(401).json({
+                 message: 'lõi'
+            });
+    } else {
+      res.status(200).json({
+                message: 'đang chạy'
+            })
+    }
+});
+
+    // const play = require('play-sound')({player: null});
+    // const soundFilePath = './Sound/hello.mp3'; 
+    // play.play(soundFilePath, (err) => {
+    //   if (err) {
+    //     res.status(401).json({
+    //         message: 'lõi'
+    //     });
+    //   } else {
+    //     res.status(200).json({
+    //         message: 'đang chạy'
+    //     })
+    //   }
+    // }); 
 });
 module.exports = router;
